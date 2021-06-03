158adf75d18ac2ec6727c43998cb006b3a667e48
Askazera <aska.pereira@gmail.com>
2021-06-03 15:13:26 -0300

    config travis

---
+++ fusion/github_api.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca um avatar de um usuário no GitHub
+
+
+    :param usuario: str com o nome de usuário no git
+    :return: str com o link do avatar
+
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(buscar_avatar('AskaVictoria'))
